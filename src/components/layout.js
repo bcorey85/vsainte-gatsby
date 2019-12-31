aa1af37dc564dbc605802c2cd86f994632e01cd5
Brandon Corey <bcorey85@gmail.com>
2019-12-31 16:59:41 -0600

    error testing for netlify deploy 5

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from './header';
+import Header from './Header';
 import Footer from './Footer';
 import PageContainer from './PageContainer';
 import Transition from './Transition';
