0915400af25c57688063b7ba4e82a216294a8e7c
Brandon Corey <bcorey85@gmail.com>
2020-02-07 18:34:31 -0600

    add threashold 75 to index img

--- src/pages/index.js
+++ src/pages/index.js
@@ -304,7 +304,9 @@ const imageQuery = graphql`
 			relativePath: { eq: "it-will-be-okay-book-cta-section.jpg" }
 		) {
 			childImageSharp {
-				fluid(traceSVG: { color: "rgb(179, 230, 225)" }) {
+				fluid(
+					traceSVG: { color: "rgb(179, 230, 225)", threshold: 75 }
+				) {
 					...GatsbyImageSharpFluid_withWebp_tracedSVG
 				}
 			}
